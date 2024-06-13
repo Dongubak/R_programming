a1dd3723af3a940bbbd38f49f2b9c2b434dcb39e
Dongubak <91736442+Dongubak@users.noreply.github.com>
2024-06-13 16:16:03 +0900

    R 기본내용
    변수
    간단한 qplot

--- ch4.R
+++ ch4.R
@@ -1,11 +1,14 @@
-english <- c(90, 80, 60, 70)
-english
+# english <- c(90, 80, 60, 70)
+# english
+# 
+# math <- c(50, 60, 100, 20)
+# math
+# 
+# class <- c(1, 1, 2, 2)
+# 
+# df_midterm <- data.frame(english, math, class)
+# df_midterm
+# 
 
-math <- c(50, 60, 100, 20)
-math
-
-class <- c(1, 1, 2, 2)
-
-df_midterm <- data.frame(english, math, class)
-df_midterm
-
+x <- c("a", "a", "b", "c")
+qplot(x)
